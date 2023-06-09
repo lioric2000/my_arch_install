b3b536f33683b2335e6e196df7a2d5564477a35b
lioric2000 <lioric2000@yandex.ru>
2023-06-10 00:16:40 +0300

    Update stage2.sh

--- stage2.sh
+++ stage2.sh
@@ -1,2 +1,14 @@
 #echo " run vim /etc/fstab  and comment all zroot lines"
 perl -pi -e 's:zpool/:#zpool/:g' /etc/fstab
+echo -e '[archzfs]
+Server = https://archzfs.com/$repo/x86_64' >> /etc/pacman.conf
+
+wget https://rchzfs.com/archzfs.gpg
+pacman-key -a archzfs.gpg
+pacman-key -r F75D9D76
+pacman-key --lsign-key F75D9D76
+
+pacman -S zfs-linux
+
+echo "/etc/mkinitcpio.conf"
+echo "HOOKS=(base udev autodetect modconf block keyboard zfs filesystems)"
