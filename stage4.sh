55abe9699ba89bb0e6956f2a3242a0eeb8883a16
lioric2000 <lioric2000@yandex.ru>
2023-06-10 00:37:37 +0300

    Create stage4.sh

---
+++ stage4.sh
@@ -0,0 +1,7 @@
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=ArchLinux
+
+grub-mkconfig -o /boot/grub/grub.cfg
+
+systemctl enable NetworkManager
+systemctl enable reflector.timer
+systemctl enable ssh
